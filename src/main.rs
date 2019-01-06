78b52bb6bcb92e5a3bcbc032dda7fdc26eaa34f8
Nicholas Yang <ny585@nyu.edu>
2019-01-06 02:18:42 -0500

    playing around with toml configs for rules

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 extern crate reqwest;
 extern crate serde_json;
+extern crate toml;
 
 use reqwest::Client;
-use serde_json::{Error, Value};
+use serde_json::Error;
+use std::fs::File;
+use std::io::Read;
+use toml::Value;
 
 static REPOS: &'static [&'static str] = &[
     "hacknyu/hacknyu-2019",
@@ -11,15 +15,34 @@ static REPOS: &'static [&'static str] = &[
 ];
 
 fn main() -> Result<(), reqwest::Error> {
+    if let Ok(contents) = read_config_file() {
+        if let Ok(config) = parse_config(contents) {
+            match config {
+                Value::Table(t) => println!("{:?}", t["rules"]),
+                _ => println!("WRONG TYPE"),
+            }
+        }
+    }
     let client = Client::new();
     for repo in REPOS {
         let url = format!("https://api.github.com/repos/{}", repo);
         let body = client.get(&url).send()?.text()?;
-        let parsedBody: Result<Value, Error> = serde_json::from_str(&body);
-        if let Ok(v) = parsedBody {
+        let parsed_body: Result<serde_json::Value, Error> = serde_json::from_str(&body);
+        if let Ok(v) = parsed_body {
             println!("{}", v["created_at"]);
         }
     }
 
     Ok(())
+}
+
+fn read_config_file() -> std::io::Result<String> {
+    let mut file = File::open("config.toml")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
+}
+
+fn parse_config(contents: String) -> Result<Value, toml::de::Error> {
+    contents.parse::<toml::value::Value>()
 }
