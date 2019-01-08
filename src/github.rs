05f17045302260762754a1d50f715b52fad50b3b
Nicholas Yang <ny585@nyu.edu>
2019-01-08 00:19:52 -0500

    simplified everything with serde

--- src/github.rs
+++ src/github.rs
@@ -1,18 +1,20 @@
 use crate::errors::Result;
 use chrono::prelude::*;
 use reqwest::Client;
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Repository {
+    created_at: String,
+}
 
 pub fn check_repos(repos: &[&'static str]) -> Result<()> {
     let client = Client::new();
     for repo in repos {
         let url = format!("https://api.github.com/repos/{}", repo);
         let body = client.get(&url).send()?.text()?;
-        let parsed_body: serde_json::Value = serde_json::from_str(&body)?;
-        let created_str = (&parsed_body["created_at"]).as_str();
-        if let Some(s) = created_str {
-            let creation_date = s.parse::<DateTime<Utc>>();
-            println!("{:?}", creation_date);
-        }
+        let repo: Repository = serde_json::from_str(&body)?;
+        println!("{:?}", repo);
     }
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #[macro_use]
 extern crate failure_derive;
+#[macro_use]
+extern crate serde_derive;
 
 use std::collections::BTreeMap;
 use std::fs::File;
